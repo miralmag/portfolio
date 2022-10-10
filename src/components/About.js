470ebaf76efc559cb7be7dcb59c47f40785cc051
miralmag <miriam.almagro@gmail.com>
2022-10-10 22:10:49 +0200

    add about content

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
 import '../styles/components/About.css';
+import foxStack from '../images/fox-stack.png';
+
 
 function AboutMe () {
     return (
         <div id='about' className='about'>
             <h2 className='title'>about the fox dev</h2>
-            <p className='message'>Hi! This is Míriam Almagro and I'm a front-end developer.</p>
-            <button className='button'><i class="fa-solid fa-code"></i>portfolio</button>
+            <p className='message'>Hi! This is Míriam Almagro and I'm a front-end developer. &#128075;</p>
+            <p className='message'>Former audiovisual translator, passionate about communication, creativity and new technologies. I define myself as hard worker, naturally curious and involved. Always willing to learn, I'm open to work in dynamic environments with new challenges to achieve.</p>
+            <p className='message'>And yes, I also love foxes.</p>
+            <p className='message'>Here is my stack:</p>
+            <img className="stack" src={foxStack} alt="stack" />
         </div>
     )
 }
