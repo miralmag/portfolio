9b0e2711613c6b900e96941e1840133c3efe508c
miralmag <miriam.almagro@gmail.com>
2022-10-10 19:23:51 +0200

    add dropdown

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef } from "react";
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import About from './About';
@@ -8,6 +9,8 @@ import Footer from './Footer';
 import '../styles/App.css';
 
 function App() {
+  
+
   return (
     <div>
       <Header />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,34 @@
 import '../styles/components/Header.css';
-import Logo from '../images/fox-logo.png'
+import Logo from '../images/fox-logo.png';
+import {useState} from 'react';
 
 function Header () {
+	const [dropDown, setDropDown] = useState ("hidden");
+	const handleClick = () => {
+		if (dropDown === "hidden") 
+		{setDropDown("")}
+		else {setDropDown("hidden")}
+	}
+	
     return (
         <header>
 		    <div className='wrapper'>
             <div className='logo'>
                 <img src={Logo} title="FoxDev" alt="FoxDev logo" className='logo-img'/>
-				{/* <h1>the fox dev</h1> */}
 				</div>
-
 			    <nav className='main-nav' >
-                    <div className='hamburguer'>
-                        <a href="#" className='ham-button'><i className="fa-solid fa-bars"></i></a>
+					<div>
+						<div className='hamburguer' onClick={handleClick}>
+							<a href="#" className='ham-button'> <i className="fa-solid fa-bars"></i></a>
+						</div>
+						
+						<div className={`dropdown--${dropDown}`}>
+							<ul>
+								<li><a href='#about'>About</a></li>
+								<li><a href='#portfolio'>Portfolio</a></li>
+								<li><a href='#contact'>Contact</a></li>
+							</ul>
+						</div>
                     </div>
 						<ul className="menu-list">
 							<li><a href="#about">about</a></li>
