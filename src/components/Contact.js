d96dac0907666c2f2a7b4da2740f13bb1c749a28
miralmag <miriam.almagro@gmail.com>
2022-10-11 13:43:15 +0200

    add first part of portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,23 +7,23 @@ function Contact () {
             <h2 className='title'>
                 contact me
             </h2>
-            <p className='contact-text'>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ut et voluptates repudiandae sint et molestiae non recusandae.</p>
+            <p className='contact-text'>Feel free to leave a message!</p>
             <form className='form' method='post' action='send.php'>
                 <p>
-                    <label className='label' htmlFor='name'>Name:</label>
-                    <input className='input' type='text' name='name' id='name'></input>
+                    <label className='label' htmlFor='name'>Name*</label>
+                    <input className='input' type='text' name='name' id='name' required placeholder='Mr. Fox'></input>
                 </p>
                 <p>
-                    <label className='label' htmlFor='email'>Email:</label>
-                    <input className='input' type='text' name='email' id='email'></input>
+                    <label className='label' htmlFor='email'>Email*</label>
+                    <input className='input' type='text' name='email' id='email' placeholder='foxdeveloper@gmail.com' required></input>
                 </p>
                 <p>
-                    <label className='label' htmlFor='subject'>Subject:</label>
-                    <input className='input' type='text' name='subject' id='subject'></input>
+                    <label className='label' htmlFor='subject'>Subject</label>
+                    <input className='input' type='text' name='subject' id='subject' ></input>
                 </p>
                 <p>
-                    <label className='label' htmlFor='message'>Message:</label>
-                    <textarea className='input' name='message' id='message' cols='45' rows='10'></textarea>
+                    <label className='label' htmlFor='message'>Message*</label>
+                    <textarea className='input' name='message' id='message' cols='45' rows='10' required ></textarea>
                 </p>
                 <div className='button-section'>
                 <button className='button'><i class="fa-solid fa-envelope"></i>submit</button>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,41 @@
 import '../styles/components/Portfolio.css';
+import openSpaces from '../images/open-spaces.mp4';
+import harryPotter from '../images/harry-potter.mp4';
+import stranger from '../images/stranger-coders.mp4';
+
 
 function Portfolio () {
     return (
         <div id='portfolio' className='portfolio'>
             <h2 className='title'>portfolio</h2>
-            <p className='text'>Échale un ojo a algunos de mis proyectos:</p>
-            <div></div>
+            <div className='video-wrapper'>
+            <article>
+            <video className="video" src={openSpaces} width="320" height="240">
+            </video>
+            <div className='icon'>
+                <i className="fa-brands fa-html5 first"></i>
+                <i className="fa-brands fa-sass second"></i>
+            </div>
+            <p className='video-title'>Open Spaces</p>
+            </article>
+
+            <article>
+            <video className="video" src={harryPotter} width="320" height="240">
+            </video>
+            <div className='icon'><i className="fa-brands fa-html5 first" ></i></div>
+            <div className='icon'><i className="fa-brands fa-react second"></i></div>
+            <div className='icon'><i className="fa-brands fa-css3-alt third"></i></div>
+            <p className='video-title'>Harry Potter Browser</p>
+            </article>
+
+            <article>
+            <video className="video" src={stranger} width="320" height="240">
+            </video>
+            <div className='icon'><i className="fa-brands fa-html5 first"></i></div>
+            <div className='icon'><i className="fa-brands fa-css3-alt second"></i></div>
+            <p className='video-title'>Stranger Coders</p>
+            </article>
+            </div>
         </div>
     )
 }
